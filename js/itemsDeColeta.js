7612a4f7628f89c9fdb1c04d1b4db1c02a35bf45
Leonardo Santos <leonardosanttos0077@gmail.com>
2020-06-05 13:38:12 -0300

    adicionei uma nova funcionalidade
    foi feita aparte do js para filtrar os items de coleta

---
+++ js/itemsDeColeta.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const itensColeta = document.querySelectorAll(".items-grid li")
+const inputColeta = $("input[name=items]")
+let idColeta = []
+
+itensColeta.forEach(item => {
+    item.addEventListener("click", ItemsColeta)
+})
+
+
+function ItemsColeta(event) {
+    let itemLi = event.target
+    itemLi.classList.toggle("selected")
+
+    let itemId = parseInt(itemLi.dataset.id)
+
+    const encontrouItens = idColeta.includes(itemId)
+    if (encontrouItens) {
+        const filtrarItens = idColeta.filter(item => item != itemId)
+        idColeta = filtrarItens
+    } else {
+        idColeta.push(itemId)
+    }
+    inputColeta.value = idColeta
+}
