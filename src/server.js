4f8e3603a6bb47b60297bfb292dd65068634c08a
Leonardo Santos <leonardosanttos0077@gmail.com>
2020-06-06 15:35:30 -0300

    html mais limpo

--- src/server.js
+++ src/server.js
@@ -3,16 +3,22 @@ const server = express()
 
 server.use(express.static("public"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    res.render("create-point.html")
 })
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    res.render("search-results.html")
 })
 
 server.listen(3000)
